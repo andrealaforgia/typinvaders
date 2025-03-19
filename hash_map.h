eec674f4442b8788472c12d652c58e535603eb1d
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-03-19 16:11:12 +0000

    Add hash map implementation

---
+++ hash_map.h
@@ -0,0 +1,28 @@
+#ifndef HASH_MAP_H_ 
+#define HASH_MAP_H_ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define table_size 100
+
+typedef struct node_t {
+    char *key;
+    int value;
+    struct node_t *next;
+} node_t, *node_ptr;
+
+typedef struct hash_map_t {
+    node_ptr table[table_size];
+} hash_map_t, *hash_map_ptr;
+
+unsigned int hash(const char *key);
+hash_map_ptr create_hash_map(void);
+void insert(hash_map_ptr map, const char *key, int value);
+int get(hash_map_ptr map, const char *key);
+void remove_key(hash_map_ptr map, const char *key);
+void free_hash_map(hash_map_ptr map);
+
+#endif
+
