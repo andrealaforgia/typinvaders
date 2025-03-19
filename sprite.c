38f47a3c7af6393c76cec183f79e656321c2156c
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-03-19 21:42:52 +0000

    Add sprite management (not tested yet)

--- sprite.c
+++ sprite.c
@@ -1,33 +1,50 @@
 #include "sprite.h"
 
+#include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "geometry.h"
 #include "graphics.h"
 
-sprite_t create_sprite(const graphics_context_ptr graphics_context,
-                       const char* texture_path) {
-  sprite_t sprite;
-  sprite.texture = IMG_LoadTexture(graphics_context->renderer, texture_path);
-  sprite.sprite_index = 0;
-  sprite.angle = 0.0;
-  if (!sprite.texture) {
-    fprintf(stderr, "Failed to load sprite texture: %s\n", IMG_GetError());
-    cleanup(&sprite);
+sprite_sheet_t create_sprite_sheet(const graphics_context_ptr graphics_context,
+                                   const char* texture_path) {
+  sprite_sheet_t sprite_sheet;
+  sprite_sheet.texture =
+      IMG_LoadTexture(graphics_context->renderer, texture_path);
+  if (!sprite_sheet.texture) {
+    fprintf(stderr, "Failed to load sprite sheet texture: %s\n",
+            IMG_GetError());
+    free_sprite_sheet(&sprite_sheet);
     exit(EXIT_FAILURE);
   }
+  return sprite_sheet;
+}
+
+void free_sprite_sheet(const sprite_sheet_ptr sprite_sheet) {
+  if (!sprite_sheet) return;
+  if (sprite_sheet->texture) {
+    SDL_DestroyTexture(sprite_sheet->texture);
+  }
+  free(sprite_sheet);
+}
+
+sprite_t create_sprite(const sprite_sheet_ptr sprite_sheet, int x, int y,
+                       int size) {
+  sprite_t sprite;
+  sprite.sheet = sprite_sheet;
+  sprite.x = x;
+  sprite.y = y;
+  sprite.size = size;
   return sprite;
 }
 
 void render_sprite(const graphics_context_ptr graphics_context,
-                   const sprite_ptr sprite, int x, int y, int sprite_size) {
-  SDL_Rect src_rect = {sprite->sprite_index * sprite_size, 0, sprite_size,
-                       sprite_size};
-  SDL_Rect dst_rect = {x, y, sprite_size, sprite_size};
-  SDL_Point center = {sprite_size / 2, sprite_size / 2};
-  SDL_RenderCopyEx(graphics_context->renderer, sprite->texture, &src_rect,
-                   &dst_rect, sprite->angle, &center, SDL_FLIP_NONE);
+                   const sprite_ptr sprite, int screen_x, int screen_y,
+                   float angle) {
+  SDL_Rect src_rect = {sprite->x, sprite->y, sprite->size, sprite->size};
+  SDL_Rect dst_rect = {screen_x, screen_y, sprite->size, sprite->size};
+  SDL_Point center = {sprite->size / 2, sprite->size / 2};
+  SDL_RenderCopyEx(graphics_context->renderer, sprite->sheet->texture,
+                   &src_rect, &dst_rect, angle, &center, SDL_FLIP_NONE);
 }
-
-void cleanup(const sprite_ptr sprite) { SDL_DestroyTexture(sprite->texture); }
--- sprite.h
+++ sprite.h
@@ -4,19 +4,26 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-#include "geometry.h"
 #include "graphics.h"
 
 typedef struct {
   SDL_Texture* texture;
-  int sprite_index;
-  double angle;
+} sprite_sheet_t, *sprite_sheet_ptr;
+
+typedef struct {
+  sprite_sheet_ptr sheet;
+  int x;
+  int y;
+  int size;
 } sprite_t, *sprite_ptr;
 
-sprite_t create_sprite(const graphics_context_ptr context,
-                       const char* texture_path);
-void render_sprite(const graphics_context_ptr context, const sprite_ptr sprite,
-                   int x, int y, int sprite_size);
-void cleanup(const sprite_ptr sprite);
+sprite_sheet_t create_sprite_sheet(const graphics_context_ptr graphics_context,
+                                   const char* texture_path);
+sprite_t create_sprite(const sprite_sheet_ptr sprite_sheet, int x, int y,
+                       int size);
+void render_sprite(const graphics_context_ptr graphics_context,
+                   const sprite_ptr sprite, int screen_x, int screen_y,
+                   float angle);
+void free_sprite_sheet(const sprite_sheet_ptr sprite_sheet);
 
 #endif  // SPRITE_H_
